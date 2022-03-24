90747743f88a01b744327c0148734f26a9477574
Nazar Klovanych <nazarn96@gmail.com>
2022-03-24 10:31:51 +0200

    Add new alias to switch instances && update and add new channels

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -3,9 +3,17 @@
         (url "https://github.com/Nazar65/guix-phps-channel")
         (introduction
          (make-channel-introduction
-          "0f4074aab0ed83f8d0ca9f5d0284db0bb45f7e99"
+          "bce6d8b460198458eb75ee3c277b8595755283de"
           (openpgp-fingerprint
            "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
+       (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (introduction
+         (make-channel-introduction
+          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+          (openpgp-fingerprint
+           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
        (channel
         (name 'my-guix-channel)
         (url "https://github.com/Nazar65/my-guix-packages-channel")
